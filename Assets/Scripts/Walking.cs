93b690ce62925984584d9543fa9959fbae7ea34e
Sonya Novikova <sona.nn@yandex.ru>
2025-03-13 01:11:25 +0300

    Walking process third edition: do better moving for all dogs

--- Assets/Scripts/Walking.cs
+++ Assets/Scripts/Walking.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
+using static UnityEngine.GraphicsBuffer;
 
 public class Walking : MonoBehaviour
 {
     private NavMeshAgent agent; // наша собака (точнее её компонент для перемещения)
     private Animator animator; // анимации собаки
+    public float WalkingSpeed = 0.5f; // скорость ходьбы собаки (можно задать в инспекторе в разделе данного скрипта)
+    public float RunningSpeed = 1f; // скорость бега собаки (можно задать в инспекторе в разделе данного скрипта)
+
     private float timer; // счётчик времени анимаций
     private float ChangeTime; // время для смены состояния
     private bool IsMoving; // параметр движения
@@ -14,44 +19,63 @@ public class Walking : MonoBehaviour
     {
         agent = GetComponent<NavMeshAgent>(); // получение компонента для перемещения
         animator = GetComponent<Animator>(); // получение компонента для управления анимациями
+
         animator.SetInteger("AnimationID", 0); // анимация дыхания (номер 0)
         IsMoving = false;
-        ChangeTime = Random.Range(4f, 10f); // случайное время до смены состояния
+        ChangeTime = 2f; // длительность состояния анимации
 
     }
     void Update()
     {
-        timer += Time.deltaTime; // повышаем счётчик
-
-        if (timer >= ChangeTime) // пора менять состояние
+        timer += Time.deltaTime; // повышаем счётчик времени
+        if (timer >= ChangeTime)
         {
-            if (IsMoving) // проверка движения
+            AnimatorStateInfo state = animator.GetCurrentAnimatorStateInfo(0); // просмотр состояния анимаций
+            if ((state.IsName("SittingStart") || state.IsName("SittingCycle")) && state.normalizedTime >= 1.0f) // если собака сидит в данный момент
             {
-                agent.isStopped = true; // остановка
-                animator.SetInteger("AnimationID", 7); // анимация сидения (номер 7)
-                IsMoving = false;
-                //animator.SetInteger("AnimationID", 0); // анимация дыхания
+                animator.SetInteger("AnimationID", 0); // встать
             }
-            else
-            {   
-                SetRandomDestination(); // собака двигается
-                int randomAnimation = Random.Range(2, 4); // случайная анимация: 2 - walking01, 2 - walking02
-                animator.SetInteger("AnimationID", randomAnimation); // установка анимации
+            if (!IsMoving) // если собака не двигалась
+            {
+                int RandomAnimation = Random.Range(2, 5); // 2 - walking01, 3 - walking02, 4 - running
+                if (RandomAnimation == 4) // если бег
+                {
+                    agent.speed = RunningSpeed; // присваиваем скорость для бега
+                }
+                else
+                {
+                    agent.speed = WalkingSpeed; // присваиваем скорость для ходьбы
+                }
+                Vector3 RandomDirection = Random.insideUnitSphere * 3f; // задаём случайное направление движения в пределах 5 метров от персонажа
+                RandomDirection += transform.position; // координата финальной точки, в которую прибудет персонаж
+                NavMeshHit point; // конкретная точка на локации
+                NavMesh.SamplePosition(RandomDirection, out point, 3f, NavMesh.AllAreas); // ищем такую точку на NavMesh
+                Vector3 direction = (point.position - transform.position).normalized; // направление к цели
+                Quaternion lookRotation = Quaternion.LookRotation(direction); // поворачиваемся к цели
+                transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 10f); // выполнение поворота
+                animator.SetInteger("AnimationID", RandomAnimation); // запуск анимации
+                agent.SetDestination(point.position); // задаём агенту цель
+                //agent.transform.Translate(Vector3.forward * agent.speed * Time.deltaTime); // движение персонажа
                 IsMoving = true;
             }
-
+            else if (IsMoving) // если собака двигалась
+            {
+                if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+                {
+                    int RandomAnimation = Random.Range(0, 2); // 0 - breathing, 2 - sitting (для рандомного выбора одного из двух, на самом деле 7 - sitting)
+                    if (RandomAnimation == 0) // дыхание
+                    {
+                        animator.SetInteger("AnimationID", 0);
+                    }
+                    else // сидение
+                    {
+                        animator.SetInteger("AnimationID", 7);
+                    }
+                    IsMoving = false;
+                }
+            }
             timer = 0; // сброс счётчика
-            ChangeTime = Random.Range(5f, 10f); // случайное время до следующей смены состояния
         }
 
-    }
-    void SetRandomDestination()
-    {
-        Vector3 RandomDirection = Random.insideUnitSphere; // выбирается случайная точка внутри сферы радиусом 1 и умножается на 10, чтобы увеличить расстояние
-        RandomDirection += transform.position; // прибавляем текущую позицию, чтобы точка была на заданном расстоянии относительно собаки
-        NavMeshHit point; // сохранение информации о точке на NavMesh
-        NavMesh.SamplePosition(RandomDirection, out point, 1, NavMesh.AllAreas); // ищем эту точку на NavMesh вокруг вычисленной точки на расстоянии <= 10
-        agent.SetDestination(point.position); // присваиваем собаке-агенту новую координату
-        agent.isStopped = false; // начинаем движение
     }
 }
