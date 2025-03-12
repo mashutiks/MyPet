0c7e9b7581dd63cae5e6989c79a9f7e6c056cb6d
Sonya Novikova <sona.nn@yandex.ru>
2025-03-12 14:33:38 +0300

    Walking process second edition: add moving in playground

--- Assets/Scripts/GoPlayground.cs
+++ Assets/Scripts/GoPlayground.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GoPlayground : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject Dog; // наша собака
+    public Button PlaygroundButton; // кнопка "Собачья площадка"
     void Start()
     {
-        
+        if (PlaygroundButton != null)
+        {
+            PlaygroundButton.onClick.AddListener(DownloadPlayground);
+        }
+        else
+        {
+            Debug.LogWarning("Кнопка 'Собачья площадка' не назначена!");
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    void DownloadPlayground()
     {
-        
+        SceneManager.LoadScene("Playground"); // загружаем сцену собачьей площадки
     }
 }
