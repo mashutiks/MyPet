c94d59ef4e846e6783f1bdfb8612f28eb7fb77f3
egsesekina <egsesekina@edu.hse.ru>
2025-05-06 14:32:53 +0300

    create own achievement from code

---
+++ Assets/MyScripts/AchievemenetManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AchievemenetManager : MonoBehaviour
+{
+    public GameObject achievementPrefab;
+
+    public Sprite[] sprites;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateAchievement("general", "Test Title", "Hello", 15, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CreateAchievement(string category, string title, string description, int points, int spriteIndex)
+    {
+        GameObject achievement = (GameObject)Instantiate(achievementPrefab);
+        SetAchievementInfo(category, achievement, title, description, points, spriteIndex);
+    }
+
+    public void SetAchievementInfo(string category, GameObject achievement, string title, string description, int points, int spriteIndex)
+    {
+        achievement.transform.SetParent(GameObject.Find(category).transform, false);
+
+        Transform titleTransform = achievement.transform.Find("title");
+        Transform descriptionTransform = achievement.transform.Find("description");
+        Transform coinsTransform = achievement.transform.Find("points");
+        Transform pictureTransform = achievement.transform.Find("picture");
+
+        if (titleTransform != null)
+            titleTransform.GetComponent<TextMeshProUGUI>().text = title;
+
+        if (descriptionTransform != null)
+            descriptionTransform.GetComponent<TextMeshProUGUI>().text = description;
+
+        if (coinsTransform != null)
+            coinsTransform.GetComponent<TextMeshProUGUI>().text = points.ToString();
+
+        if (pictureTransform != null)
+            pictureTransform.GetComponent<Image>().sprite = sprites[spriteIndex];
+
+    }
+}
